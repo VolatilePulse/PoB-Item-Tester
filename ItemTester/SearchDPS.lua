48b95ce9ab6fe681fe7636a024aeac608f87c686
Ant Purchase <antoranthony.purchase@gmail.com>
2018-10-18 19:52:20 +0100

    Use flask for DPS search
    Simplifies code significantly.
    
    Output is now JavaScript to paste into website.
    In future, output will be simple URL with query parameters once website supports it.

--- ItemTester/SearchDPS.lua
+++ ItemTester/SearchDPS.lua
@@ -1,11 +1,9 @@
 HELP = [[
     To use this tool:
 
-    1) Ensure you have an empty jewel socket in PoB and you have saved your build.
-    2) Run this script with your build XML file as the first argument.
-    3) Take the output weights and paste them into GenerateSearch.py.
-    4) Set build-specific terms in 'selections' in GenerateSearch.py.
-    5) Run GenerateSearch.py and use the generated URL.
+    1) Ensure you have saved your build in PoB.
+    2) On the command-line run: "Path of Building.exe" ItemTester\SearchDPS.lua Builds\<buildname>.xml
+    3) Take the output and paste it into the JavaScript console (F12) of http://gw2crafts.net/pobsearch/modsearch.html.
 ]]
 
 
@@ -21,90 +19,66 @@ xml = require("xml")
 inspect = require("inspect")
 
 
-function findSlotToUse()
-    -- A fake item to narrow the slots down
-    local modLine = "+100 to Accuracy Rating"
-    local itemText = "Rarity: Rare\n+100 to Accuracy Rating\nHypnotic Eye Jewel\n--------\nItem Level: 1\n--------\n+100 to Accuracy Rating"
+function findModEffect(modLine)
+    -- Construct flask with the mod
+    local itemText = "Test Item\nAmethyst Flask\n"..modLine
     local item = common.New("Item", build.targetVersion, itemText)
-
-    local compareSlots = { }
-    for slotName, slot in pairs(build.itemsTab.slots) do
-        if build.itemsTab:IsItemValidForSlot(item, slotName) and not slot.inactive and (not slot.weaponSet or slot.weaponSet == (build.itemsTab.activeItemSet.useSecondWeaponSet and 2 or 1)) then
-            t_insert(compareSlots, slot)
-        end
-    end
-
-    -- Sort empty or lowest-numbered sockets first
-    table.sort(compareSlots, function(a, b)
-        if a.selItemId ~= b.selItemId then
-            if a.selItemId == 0 then
-                return true
-            elseif b.selItemId == 0 then
-                return false
-            elseif item == build.itemsTab.items[a.selItemId] then
-                return true
-            elseif item == build.itemsTab.items[b.selItemId] then
-                return false
-            end
-        end
-        local aNum = tonumber(a.slotName:match("%d+"))
-        local bNum = tonumber(b.slotName:match("%d+"))
-        if aNum and bNum then
-            return aNum < bNum
-        else
-            return a.slotName < b.slotName
-        end
-    end)
-
-    -- First is best
-    return compareSlots[1]
-end
-
-function findModEffect(modLine, slot)
-    -- Construct jewel with the mod
-    local itemText = "Rarity: Rare\n"..modLine.."\n".."Hypnotic Eye Jewel\n--------\nItem Level: 1\n--------\n"..modLine
-    local item = common.New("Item", build.targetVersion, itemText)
-    item:NormaliseQuality()
-    item:BuildModList()
 
     -- Calculate stat differences
-    local calcFunc, calcBase = build.calcsTab:GetMiscCalculator()
-    build.itemsTab:UpdateSockets()
-    local selItem = build.itemsTab.items[slot.selItemId]
-    local compareBase = calcFunc({ repSlotName = slot.slotName, repItem = item ~= selItem and item })
+    local calcFunc, baseStats = build.calcsTab:GetMiscCalculator()
+    local newStats = calcFunc({ toggleFlask = item })
 
     -- Pull out the difference in Total DPS
-    local statVal1 = compareBase.TotalDPS or 0
-    local statVal2 = calcBase.TotalDPS or 0
+    local statVal1 = newStats.TotalDPS or newStats.AverageHit or 0
+    local statVal2 = baseStats.TotalDPS or baseStats.AverageHit or 0
     local diff = statVal1 - statVal2
 
     return diff
 end
 
+modData = {
+    { id="flat accuracy", scale="100", text="+100 to Accuracy Rating" },
+    { id="% accuracy", scale="10", text="10% increased Global Accuracy Rating" },
+    { id="% physical", scale="12", text="12% increased Physical Damage" },
+    { id="% lightning", scale="12", text="12% increased Lightning Damage" },
+    { id="% cold", scale="12", text="12% increased Cold Damage" },
+    { id="% fire", scale="12", text="12% increased Fire Damage" },
+    { id="% elemental", scale="12", text="12% increased Elemental Damage" },
+    { id="% chaos", scale="12", text="12% increased Chaos Damage" },
+    { id="% generic", scale="12", text="12% increased Damage" },
+    { id="crit chance", scale="16", text="16% increased Global Critical Strike Chance" },
+    { id="crit multi", scale="16", text="+16% to Global Critical Strike Multiplier" },
+    { id="attack speed", scale="10", text="10% increased Attack Speed" },
+    { id="cast speed", scale="10", text="10% increased Cast Speed" },
+    { id="pen all", scale="8", text="Damage Penetrates 8% Elemental Resistances" },
+    { id="pen lightning", scale="8", text="Damage Penetrates 8% Lightning Resistance" },
+    { id="pen cold", scale="8", text="Damage Penetrates 8% Cold Resistance" },
+    { id="pen fire", scale="8", text="Damage Penetrates 8% Fire Resistance" },
+    { id="flat phys", scale="6", text="Adds 5 to 7 Physical Damage" },
+    { id="flat lightning", scale="37", text="Adds 6 to 68 Lightning Damage" },
+    { id="flat cold", scale="22.5", text="Adds 14 to 31 Cold Damage" },
+    { id="flat fire", scale="27.5", text="Adds 21 to 34 Fire Damage" },
+    { id="flat chaos", scale="5.5", text="Adds 4 to 7 Chaos Damage" },
+    { id="extra lightning", scale="15", text="15% of Physical Damage as Extra Lightning Damage" },
+    { id="extra cold", scale="15", text="15% of Physical Damage as Extra Cold Damage" },
+    { id="extra fire", scale="15", text="15% of Physical Damage as Extra Fire Damage" },
+    { id="extra chaos", scale="15", text="Gain 15% of Non-Chaos damage as Extra Chaos Damage" },
+    { id="ele as chaos", scale="15", text="Gain 15% of Elemental Damage as Extra Chaos Damage" },
+    { id="+1 power charge", scale="1", text="+1 to Maximum Power Charges" },
+    { id="+1 frenzy charge", scale="1", text="+1 to Maximum Frenzy Charges" },
+    { id="+1 endurance charge", scale="1", text="+1 to Maximum Endurance Charges" },
+}
 
 
 -- Load a build file
 local buildXml = loadText(BUILD_XML)
 loadBuildFromXML(buildXml)
 
--- Load the DPS mod data
-local mods = xml.LoadXMLFile("ItemTester/mods.xml")
-
--- Work out which slot to use
-local slot = findSlotToUse()
-print("Using slot "..slot.label)
-print()
-
--- Get DPS difference for each mod
+-- Get DPS difference for each mod and output
+print('f=function(n,v){document.getElementsByName(n)[0].value=v}')
 local modEffects = {}
-for _,mod in ipairs(mods[1]) do
-    local dps = findModEffect(mod[1], slot)
-    dps = dps / tonumber(mod.attrib.scale)
-    modEffects[mod.attrib.id] = dps
-end
-modEffects["extra random"] = (modEffects["extra fire"] + modEffects["extra cold"] + modEffects["extra lightning"]) / 3
-
--- Save mod effects
-for id,dps in pairs(modEffects) do
-    print(string.format("\t\t\"%s\": %.1f,", id, dps))
+for _,mod in ipairs(modData) do
+    local dps = findModEffect(mod.text)
+    -- dps = dps / tonumber(mod.scale) -- only needed if inputting to the original Python script
+    print(string.format("f('%s','%.1f')", mod.id, dps))
 end
