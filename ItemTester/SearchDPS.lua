4c73989f20e6b6c5f560e8a55e336a7ae584af3d
Ant Purchase <antoranthony.purchase@gmail.com>
2018-11-23 11:14:41 +0000

    Use DPS mods from mods.json
    Manual update with `updatemods.bat`.
    In future this should update like the Lua scripts.

--- ItemTester/SearchDPS.lua
+++ ItemTester/SearchDPS.lua
@@ -15,6 +15,7 @@ local _,_,SCRIPT_PATH=string.find(arg[0], "(.+[/\\]).-")
 dofile(SCRIPT_PATH.."mockui.lua")
 
 xml = require("xml")
+json = require("json")
 inspect = require("inspect")
 
 debug = false
@@ -99,61 +100,13 @@ function encodeValue(value)
     end
 end
 
-
-modData = {
-    {name="% dot", desc="12% increased Damage over Time", count=12},
-    {name="% bleed", desc="12% increased Damage with Bleeding", count=12},
-    {name="% poison", desc="12% increased Damage with Poison", count=12},
-    {name="% ignite", desc="12% increased Burning Damage", count=12},
-    {name="flat accuracy", desc="+100 to Accuracy Rating", count=100},
-    {name="% accuracy", desc="10% increased Global Accuracy Rating", count=10},
-    {name="% physical", desc="12% increased Physical Damage", count=12},
-    {name="% lightning", desc="12% increased Lightning Damage", count=12},
-    {name="% cold", desc="12% increased Cold Damage", count=12},
-    {name="% fire", desc="12% increased Fire Damage", count=12},
-    {name="% elemental", desc="12% increased Elemental Damage", count=12},
-    {name="% chaos", desc="12% increased Chaos Damage", count=12},
-    {name="% generic", desc="12% increased Damage", count=12},
-    {name="crit chance", desc="16% increased Global Critical Strike Chance", count=16},
-    {name="crit multi", desc="+16% to Global Critical Strike Multiplier", count=16},
-    {name="attack speed", desc="10% increased Attack Speed", count=10},
-    {name="cast speed", desc="10% increased Cast Speed", count=10},
-    {name="pen all", desc="Damage Penetrates 8% Elemental Resistances", count=8},
-    {name="pen lightning", desc="Damage Penetrates 8% Lightning Resistance", count=8},
-    {name="pen cold", desc="Damage Penetrates 8% Cold Resistance", count=8},
-    {name="pen fire", desc="Damage Penetrates 8% Fire Resistance", count=8},
-    {name="flat phys", desc="Adds 5 to 7 Physical Damage", count=6},
-    {name="flat lightning", desc="Adds 6 to 68 Lightning Damage", count=37},
-    {name="flat cold", desc="Adds 14 to 31 Cold Damage", count=22.5},
-    {name="flat fire", desc="Adds 21 to 34 Fire Damage", count=27.5},
-    {name="flat chaos", desc="Adds 4 to 7 Chaos Damage", count=5.5},
-    {name="extra lightning", desc="15% of Physical Damage as Extra Lightning Damage", count=15},
-    {name="extra cold", desc="15% of Physical Damage as Extra Cold Damage", count=15},
-    {name="extra fire", desc="15% of Physical Damage as Extra Fire Damage", count=15},
-    {name="extra chaos", desc="Gain 15% of Non-Chaos damage as Extra Chaos Damage", count=15},
-    {name="ele as chaos", desc="Gain 15% of Elemental Damage as Extra Chaos Damage", count=15},
-    {name="lightning as extra chaos", desc="15% of Lightning Damage as Extra Chaos Damage", count=15},
-    {name="cold as extra chaos", desc="15% of Cold Damage as Extra Chaos Damage", count=15},
-    {name="fire as extra chaos", desc="15% of Fire Damage as Extra Chaos Damage", count=15},
-    {name="+1 power charge", desc="+1 to Maximum Power Charges", count=1},
-    {name="+1 frenzy charge", desc="+1 to Maximum Frenzy Charges", count=1},
-    {name="+1 endurance charge", desc="+1 to Maximum Endurance Charges", count=1},
-    {name="20 dex", desc="+20 to Dexterity", count=20},
-    {name="20 int", desc="+20 to Intelligence", count=20},
-    {name="20 str", desc="+20 to Strength", count=20},
-    {name="damage per dex", desc="1% increased Damage per 15 Dexterity", count=1},
-    {name="damage per int", desc="1% increased Damage per 15 Intelligence", count=1},
-    {name="damage per str", desc="1% increased Damage per 15 Strength", count=1},
-    {name="% dex", desc="+10% Dexterity", count=10},
-    {name="% int", desc="+10% Intelligence", count=10},
-    {name="% str", desc="+10% Strength", count=10},
-    {name="% lowest", desc="1% increased Damage per 5 of your lowest Attribute", count=1},
-    {name="% life", desc="7% increased maximum Life", count=7},
-    {name="flat life", desc="+40 to maximum Life", count=40},
-    {name="% es", desc="7% increased maximum Energy Shield", count=7},
-    {name="flat es", desc="+40 to maximum Energy Shield", coun= 40}
-}
-
+local modDataText = loadText(SCRIPT_PATH.."mods.json")
+if modDataText then
+    modData = json.decode(modDataText)
+else
+    print("Error: Failed to load mods.json")
+    os.exit(1)
+end
 
 -- Load a specific build file or use the default
 if BUILD_XML ~= "CURRENT" then
---
+++ updatemods.bat
@@ -0,0 +1,3 @@
+@echo off
+
+curl -o ItemTester\mods.json "https://raw.githubusercontent.com/xanthics/PoE_Weighted_Search/master/mods.json"
